9a0aba1ea41f7bc1395f5a175f5e124636239c26
nieledm <nieledm@gmail.com>
2025-03-03 11:30:41 -0300

    Exercício 1 - ok

---
+++ nieleMendes_targetTest.py
@@ -0,0 +1,14 @@
+#Questão 1
+
+#Declaração de variáveis
+indice = 13
+soma = 0
+k = 0
+
+#Laço para somar
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+#Impressão do resultado no terminal
+print (soma)
