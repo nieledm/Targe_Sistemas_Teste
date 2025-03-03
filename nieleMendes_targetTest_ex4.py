d65ae210adba8f64c1bf0c466a82246d1689a0d7
nieledm <nieledm@gmail.com>
2025-03-03 11:58:29 -0300

    Exercício 4 - ok

---
+++ nieleMendes_targetTest_ex4.py
@@ -0,0 +1,17 @@
+#Cadastro de dados, organizados em um dicionário
+faturamento_estados = {
+    'sp' : 67836.43,
+    'rj': 36678.66,
+    'mg': 29229.88,
+    'es': 27165.48,
+    'outros': 19849.53
+}
+
+total = sum(faturamento_estados.values())
+
+print("O percentual de representação que cada estado foi:")
+
+#Laço for para impressão das porcentagens das participações
+for estado, valor in faturamento_estados.items():
+    percentual = (valor / total) * 100
+    print (f"{estado}: {percentual:.2f}%")
